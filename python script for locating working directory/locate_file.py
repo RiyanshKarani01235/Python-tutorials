49350110eea4b4f6029e4e59efc61b747a2f5d97
ironstein <ironstein1994@gmail.com>
2015-12-20 00:03:37 +0530

    edit for locate_file.py

--- python script for locating working directory/locate_file.py
+++ python script for locating working directory/locate_file.py
@@ -1,10 +1,5 @@
-def locate_file() :
-    working_directory = __file__.split('/')[:-1]
-    return_string = ''
-    for string in working_directory :
-        return_string += string
-        return_string += '/'
-
-    return return_string
-
-locate_file()
+def locate_file() : 
+    working_directory = ''
+    for element in __file__.split('/')[:-1] :
+        working_directory += element + '/'
+    return working_directory
